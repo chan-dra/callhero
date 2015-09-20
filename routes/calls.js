60f7ab2b981950caa89a157f45467308851a8348
Chandra Bhavanasi <chandra.bhavanasi@isocket.com>
2015-09-20 00:03:32 -0700

    add call support

--- routes/calls.js
+++ routes/calls.js
@@ -28,15 +28,37 @@ var findUser = function(id, db, callback) {
 router.get('/', function(req, res, next) {
     var id = '2705621411';
     var resp = new twilio.TwimlResponse();
-    console.log(req.body);
-    console.log(req.query);
     MongoClient.connect(url, function(err, db) {
         findUser(id, db, function(err, user) {
             var rules = user.rules;
             for(var i = 0; i < rules.length; i++) {
                 var rule = rules[i];
                 if (rule.type === 'call') {
-                    resp.say({voice:'woman'}, 'ahoy hoy! Testing Twilio and node.js');
+                    //resp.say({voice:'woman'}, 'ahoy hoy! Testing Twilio and node.js');
+                    if (rule.rule === 'not_in_contacts') {
+                        var isCallerInContacts = false;
+                        var contacts = user.contacts;
+                        if (req.query.From) {
+                            var fromNumber = req.query.From;
+                            fromNumber = fromNumber.split('+1')[1];
+                            var isCallerInContacts = false;
+                            for (var j = 0; i < contacts.length; j++) {
+                                var contact = contacts[j];
+                                if (contact.phoneNumber === fromNumber) {
+                                    isCallerInContacts = true;
+                                    break;
+                                }
+                            }
+
+                        }
+                        if (rule.action === 'drop') {
+                            if (isCallerInContacts) {
+                                resp.say({voice:'woman'}, 'You are in contacts');
+                            } else {
+                                resp.say({voice:'woman'}, 'You are not in contacts, call is going to drop');
+                            }
+                        }
+                    }
                 }
             }
             res.writeHead(200, {
