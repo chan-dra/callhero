60e555bc26a80c6d30ecb8241c391e5abcc2959a
Chandra Bhavanasi <chandra.bhavanasi@isocket.com>
2015-09-19 23:45:37 -0700

    add twilio

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var calls = require('./routes/calls');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/calls', calls);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/calls.js
@@ -0,0 +1,48 @@
+/**
+ * Created by chandra on 9/19/15.
+ */
+var express = require('express');
+var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://localhost:27017/test';
+var Step = require('step');
+var twilio = require('twilio');
+
+var findUser = function(id, db, callback) {
+    console.log(id);
+    var cursor = db.collection('users').find({phoneNumber: id});
+    Step(
+        function() {
+            var grp = this.group()
+            cursor.each(grp())
+        },
+        function (err, users) {
+            console.log(users);
+            callback(err, users[0]);
+        }
+    )
+};
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    var id = '2705621411';
+    var resp = new twilio.TwimlResponse();
+    MongoClient.connect(url, function(err, db) {
+        findUser(id, db, function(err, user) {
+            var rules = user.rules;
+            for(var i = 0; i < rules.length; i++) {
+                var rule = rules[i];
+                if (rule.type === 'call') {
+                    resp.say({voice:'woman'}, 'ahoy hoy! Testing Twilio and node.js');
+                }
+            }
+            res.writeHead(200, {
+                'Content-Type':'text/xml'
+            });
+            res.end(resp.toString());
+        });
+    });
+});
+
+module.exports = router;
